727511e4acbbf7d05cd94690267c22b7dc64aa06
devH <21522095@gm.uit.edu.vn>
2024-05-15 08:36:02 +0700

    init assets

--- client/src/components/SettingGroup/index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './SettingGroup';
-export { default as FormItem } from './FormItem';
-export { default as GroupInfoItem } from './GroupItem';
--- client/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const withReport = process.env.npm_config_withReport;
-
-module.exports = {
-  plugins: [withReport ? new BundleAnalyzerPlugin() : ''],
-};
